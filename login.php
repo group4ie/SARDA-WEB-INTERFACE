d9fca88bb3ae8f8e2acf14162c166f77d6c3a49f
akodinye <aokod1@student.monash.edu>
2015-04-24 15:36:16 +0200

    Repository Used to store information for our IE project
    Repository Used to store information for our IE project
    
    The Sarda
    
    Communicator is an android application used to share information between
    staff and Volunteers.

---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+
+print "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n";
+print "<head>\n";
+print "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
+print " <link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"loginstyle.css\"/>\n";
+print "<title>Log-in</title>\n";
+print "</head>\n";
+print "\n";
+print "\n";
+print "<body>\n";
+print "<div id='header'>\n";
+print "</div>\n";
+print "<div id=\"login_block\">\n";
+print "\n";
+print "\n";
+print "<h1> Log-in </h1>\n";
+print "<form id='login' >\n";
+print "\n";
+print "	\n";
+print "	<input type='text' name='email' id='email' placeholder=\"Email\" />\n";
+print "    \n";
+print "	<input type='password' name='password' id='password' placeholder=\"Password\" />\n";
+print "	\n";
+print "    <input type='submit' name='login' id=\"login_button\" value='Login' />\n";
+print " \n";
+print "</form>  \n";
+print "</div>\n";
+print "\n";
+print "\n";
+print " <div id=\"footer\">\n";
+print "\n";
+print "             <p> 2015- SARDA GAUTENG</p> \n";
+print "    \n";
+print "            </div>\n";
+print "\n";
+print "\n";
+print "</body>\n";
+print "</html>";
+
+?>
---
+++ webpages copy/LoginPage.php
@@ -0,0 +1,43 @@
+<?php
+
+echo "\n";
+echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n";
+echo "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n";
+echo "<head>\n";
+echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
+echo " <link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"loginstyle.css\"/>\n";
+echo "<title>Log-in</title>\n";
+echo "</head>\n";
+echo "\n";
+echo "\n";
+echo "<body>\n";
+echo "<div id='header'>\n";
+echo "</div>\n";
+echo "<div id=\"login_block\">\n";
+echo "\n";
+echo "\n";
+echo "<h1> Log-in </h1>\n";
+echo "<form id='login' >\n";
+echo "\n";
+echo "	\n";
+echo "	<input type='text' name='email' id='email' placeholder=\"Email\" />\n";
+echo "    \n";
+echo "	<input type='password' name='password' id='password' placeholder=\"Password\" />\n";
+echo "	\n";
+echo "    <input type='submit' name='login' id=\"login_button\" value='Login' />\n";
+echo " \n";
+echo "</form>  \n";
+echo "</div>\n";
+echo "\n";
+echo "\n";
+echo " <div id=\"footer\">\n";
+echo "\n";
+echo "             <p> 2015- SARDA GAUTENG</p> \n";
+echo "    \n";
+echo "            </div>\n";
+echo "\n";
+echo "\n";
+echo "</body>\n";
+echo "</html>";
+
+?>
